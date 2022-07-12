2e346652f5cda9ab3795ca123e67e17b5f7ec3ae
Annie Jung <younjujung27@gmail.com>
2022-07-11 17:40:21 -0700

    first commit

---
+++ reverseString/index.js
@@ -0,0 +1,71 @@
+// Directions
+// Return a string with the order of characters reversed
+// --- Examples
+//   reverse('abcd') === 'dcba'
+//   reverse('Hello!') === '!olleH'
+
+function reverse(str) {
+  /* solution 1 (using built in methods)
+1. turn the string into an array (use .split(''))
+2. use array method of reverse() to reverse contents of array
+3. turn the reversed array back into a string (using .join(""))
+  //   let arr = str.split("");
+  //   arr.reverse();
+  //   const res = arr.join("");
+  //   return res;
+*/
+
+  /* Solution 2 - solve without using built in methods
+1. create empty string variable.
+2. iterate through the str using for loop
+3. concat each letter (str[i]) and result string.
+*/
+  let res = "";
+
+  for (let i = 0; i < str.length; i++) {
+    const letter = str[i];
+    res = letter + res;
+  }
+
+  return res;
+}
+
+/* 
+Time Complexity : O(N)  We go over every character in Input String
+Space Complexity : O(N)
+*/
+
+// _________ _______  _______ _________   _______  _______  _______  _______  _______
+// \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
+//    ) (   | (    \/| (    \/   ) (     | (    \/| (   ) || (    \/| (    \/| (    \/
+//    | |   | (__    | (_____    | |     | |      | (___) || (_____ | (__    | (_____
+//    | |   |  __)   (_____  )   | |     | |      |  ___  |(_____  )|  __)   (_____  )
+//    | |   | (            ) |   | |     | |      | (   ) |      ) || (            ) |
+//    | |   | (____/\/\____) |   | |     | (____/\| )   ( |/\____) || (____/\/\____) |
+//    )_(   (_______/\_______)   )_(     (_______/|/     \|\_______)(_______/\_______)
+//                             ____       _
+//                             |  _ \     | |
+//                             | |_) | ___| | _____      __
+//                             |  _ < / _ \ |/ _ \ \ /\ / /
+//                             | |_) |  __/ | (_) \ V  V /
+//                             |____/ \___|_|\___/ \_/\_/
+//                         ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+//                          ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+//                          ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+mocha.setup("bdd");
+const { assert } = chai;
+
+describe("String Reversal", () => {
+  it("reverse() correctly reverses string", () => {
+    assert.equal(reverse("ffaa"), "aaff");
+    assert.equal(reverse("  aaff"), "ffaa  ");
+  });
+});
+
+mocha.run();
