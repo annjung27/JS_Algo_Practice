c37f06a853e0b94681c2f9eb73d9340f3760b989
Annie Jung <younjujung27@gmail.com>
2022-08-03 11:42:09 -0700

    added solution and notes

--- linearSearch/index.js
+++ linearSearch/index.js
@@ -6,11 +6,51 @@
 // linearSearchIncludes(foods, "pizza") --> true
 // linearSearchFind(foods, food => food === "pizza") --> "pizza"
 
-function linearSearchIndexOf(arr, val) {}
+function linearSearchIndexOf(arr, val) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      return i;
+    }
+  }
+  return -1;
 
-function linearSearchIncludes(arr, val) {}
+  // using built in method:
+  // return arr.indexOf(val);
+}
 
-function linearSearchFind(arr, cb) {}
+function linearSearchIncludes(arr, val) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      return true;
+    }
+  }
+  return false;
+
+  // using builtin method:
+  // reutrn arr.includes(val);
+}
+
+function linearSearchFind(arr, cb) {
+  for (let i = 0; i < arr.length; i++) {
+    if (cb(arr[i]) === true) {
+      return arr[i];
+    }
+  }
+  return undefined;
+
+  // using builtin method:
+  // return arr.find(cb);
+}
+
+/**
+ Time Complexity for Lnear Search: O(N)
+ because our search algorithm in the worst case needs to iterate over every item in Array 
+ to check if it is the value we are looking for. 
+
+ O(N) / Linear Time Complexity is best we can do for searching an "unsorted" array. 
+ On a "sorted" array we can get much better time complexity with Binary Search.
+
+ */
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
@@ -51,7 +91,7 @@ describe("linearSearchIndexOf()", () => {
   });
 });
 
-describe.skip("linearSearchIncludes()", () => {
+describe("linearSearchIncludes()", () => {
   it("returns true if item found", () => {
     assert.equal(linearSearchIncludes(foods, "pizza"), true);
   });
@@ -60,17 +100,17 @@ describe.skip("linearSearchIncludes()", () => {
   });
 });
 
-describe.skip("linearSearchFind()", () => {
+describe("linearSearchFind()", () => {
   it("returns item back if found", () => {
     const kevin = people[0];
     assert.equal(
-      linearSearchFind(people, person => person.name === "Kevin"),
+      linearSearchFind(people, (person) => person.name === "Kevin"),
       kevin
     );
   });
   it("returns undefined if item NOT found", () => {
     assert.equal(
-      linearSearchFind(people, person => person.name === "Kevinn"),
+      linearSearchFind(people, (person) => person.name === "Kevinn"),
       undefined
     );
   });
