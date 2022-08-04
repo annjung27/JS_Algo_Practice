dee44e7c8d70abfc5475cb687fbc3d99716432d7
Annie Jung <younjujung27@gmail.com>
2022-08-04 13:32:04 -0700

    solution added

--- binarySearch/index.js
+++ binarySearch/index.js
@@ -2,7 +2,30 @@
 // for the provided value.
 // The Binary Search Algorithm will return the index of value found, or -1 if not found.
 
-const binarySearch = (sortedArr, value) => {};
+const binarySearch = (sortedArr, value) => {
+  let left = 0;
+  let right = sortedArr.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const midVal = sortedArr[mid];
+    if (midVal === value) {
+      return mid;
+    } else if (midVal < value) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+};
+
+/*
+Time Complexity for Binary Search : O(log n)
+If N is doubled, we only have to do one more operation.
+This is because with each operation, we can elimiate half of remaining array. 
+Binary Seach only works on sorted array.
+*/
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
