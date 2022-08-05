396a03f54d1c657ff5dfc52ca09243d41df6432e
Annie Jung <younjujung27@gmail.com>
2022-08-05 15:19:07 -0700

    solution added

--- stringPatternSearch/index.js
+++ stringPatternSearch/index.js
@@ -3,7 +3,29 @@
 // stringPatternSearch("lollipop", "lol") --> 1
 // stringPatternSearch("lolol", "lol") --> 2
 
-function stringPatternSearch(text, pattern) {}
+function stringPatternSearch(text, pattern) {
+  let count = 0;
+
+  for (let i = 0; i < text.length; i++) {
+    for (let j = 0; j < pattern.length; j++) {
+      const patternChar = pattern[j];
+      if (patternChar !== text[i + j]) {
+        break;
+      }
+      if (j === pattern.length - 1) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+/**
+ Time Complexity : O(N*M)
+ because for each character in text, go over every character in Pattern.
+
+ Space Complexity : O(1)
+ */
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
